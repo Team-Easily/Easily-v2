ab8652bb8b847d8d5e4d8cdb87ab54ea53f137c1
Kathryn McCrum <kathryncmccrum@gmail.com>
2022-08-24 09:36:39 -0400

    todoslice added

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './components/auth/authSlice';
+import todosReducer from './components/todos/todoSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    todos: todosReducer,
   },
 });
 
