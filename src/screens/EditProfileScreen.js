98beffb2f452f442d3e7cb20f3042d8fe7eb7c7e
Kimberly Ma <Kimberly_Ma@Yahoo.com>
2022-08-28 13:14:19 -0400

    trying to edit text for edit user

--- src/screens/EditProfileScreen.js
+++ src/screens/EditProfileScreen.js
@@ -1,41 +1,80 @@
 import React, { useEffect, useState } from "react";
 import { doc, updateDoc, getDoc } from "firebase/firestore";
-import { View, Text, Button, StyleSheet, Input } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { Button, TextInput } from "react-native-paper";
 import { auth, db } from "../firebase/firebase";
-import { getUser, setUser } from "./ProfileScreen";
+import { NavigationHelpersContext } from "@react-navigation/native";
 
 export const EditProfileScreen = () => {
-  const [user] = useState({});
+  const [user, setUser] = useState({});
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [address, setAddress] = useState("");
+
+  const getUser = async () => {
+    const docSnap = await getDoc(doc(db, "users", auth.currentUser.uid));
+    if (docSnap.exists()) {
+      setUser(docSnap.data());
+    } else {
+      console.log("No such document!");
+    }
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
+
   console.log(user);
 
   const handleUpdate = async () => {
     try {
       await updateDoc(doc(db, "users", auth.currentUser.uid), {
         userName: userName,
+        firstName: firstName,
+        lastName: lastName,
         address: address,
       });
     } catch (error) {
       console.error(error);
-    } finally {
-      getUser();
     }
+    // finally {
+    //   getUser();
+    // }
   };
-
-  useEffect(() => {
-    getUser();
-  }, []);
 
   return (
     <View style={styles.container}>
       <Text>Edit Profile</Text>
       <View>
-        <Input
+        <TextInput
           value={user.userName}
           placeholder="userName"
           onChangeText={(text) => {
-            handleUpdate(text);
+            setUserName(text);
           }}
         />
+        {/* <TextInput
+          value={user.firstName}
+          placeholder="firstName"
+          onChangeText={(text) => {
+            setUserName(text);
+          }}
+        />
+        <TextInput
+          value={user.lastName}
+          placeholder="lastName"
+          onChangeText={(text) => {
+            setUserName(text);
+          }}
+        /> */}
+        <TextInput
+          value={user.address}
+          placeholder="address"
+          onChangeText={(text) => {
+            setAddress(text);
+          }}
+        />
+        <Button onPress={handleUpdate}>Update</Button>
       </View>
       <Button title="ClickHere" onPress={() => alert("Button Clicked!")} />
     </View>
