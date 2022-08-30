cf4994f518fdab8a0a03a9a34a45384bbd9f7274
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-08-30 12:28:23 -0500

    prettified

--- src/screens/EditProfileScreen.js
+++ src/screens/EditProfileScreen.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { doc, updateDoc, getDoc, setDoc } from "firebase/firestore";
-import { View, Text, StyleSheet } from "react-native";
-import { Button, TextInput } from "react-native-paper";
-import { auth, db } from "../firebase/firebase";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { doc, updateDoc, getDoc, setDoc } from 'firebase/firestore';
+import { SafeAreaView, View, StyleSheet } from 'react-native';
+import { Headline, Button, TextInput } from 'react-native-paper';
+import { auth, db } from '../firebase/firebase';
+import { useSelector } from 'react-redux';
 
 export const EditProfileScreen = ({ navigation }) => {
   const userUid = useSelector((state) => state.auth.currentUserUid);
   const [user, setUser] = useState({});
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [userName, setUserName] = useState("");
-  const [address, setAddress] = useState("");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [address, setAddress] = useState('');
 
   const getUser = async () => {
-    const docSnap = await getDoc(doc(db, "users", userUid));
+    const docSnap = await getDoc(doc(db, 'users', userUid));
     if (docSnap.exists()) {
       setUser(docSnap.data());
       console.log(docSnap.data());
     } else {
-      console.log("No such document!");
+      console.log('No such document!');
     }
   };
   useEffect(() => {
@@ -28,80 +28,107 @@ export const EditProfileScreen = ({ navigation }) => {
 
   const handleUpdate = async () => {
     try {
-      if (userName !== "") {
-        await updateDoc(doc(db, "users", userUid), { userName: userName });
+      if (userName !== '') {
+        await updateDoc(doc(db, 'users', userUid), { userName: userName });
       }
-      if (firstName !== "") {
-        await updateDoc(doc(db, "users", userUid), { firstName: firstName });
+      if (firstName !== '') {
+        await updateDoc(doc(db, 'users', userUid), { firstName: firstName });
       }
-      if (lastName !== "") {
-        await updateDoc(doc(db, "users", userUid), { lastName: lastName });
+      if (lastName !== '') {
+        await updateDoc(doc(db, 'users', userUid), { lastName: lastName });
       }
-      if (address !== "") {
-        await updateDoc(doc(db, "users", userUid), { address: address });
+      if (address !== '') {
+        await updateDoc(doc(db, 'users', userUid), { address: address });
       }
     } catch (error) {
       console.error(error);
     } finally {
       getUser();
-      navigation.push("Profile");
+      navigation.push('Profile');
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Edit Profile</Text>
-      <View>
-        <TextInput
-          value={userName}
-          placeholder={user.userName ? user.userName : "username"}
-          onChangeText={(text) => {
-            setUserName(text);
-          }}
-        />
-        <TextInput
-          value={firstName}
-          placeholder={user.firstName ? user.firstName : "First Name"}
-          onChangeText={(text) => {
-            setFirstName(text);
-          }}
-        />
-        <TextInput
-          value={lastName}
-          placeholder={user.lastName ? user.lastName : "Last Name"}
-          onChangeText={(text) => {
-            setLastName(text);
-          }}
-        />
-        <TextInput
-          value={address}
-          placeholder={user.address ? user.address : "Address"}
-          onChangeText={(text) => {
-            setAddress(text);
-          }}
-        />
-        <Button onPress={handleUpdate}>Update</Button>
+    <SafeAreaView style={styles.container}>
+      <View style={{ paddingHorizontal: 30 }}>
+        <View style={{ alignItems: 'center' }}>
+          <Headline>Edit Profile</Headline>
+        </View>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            value={userName}
+            placeholder={user.userName ? user.userName : 'username'}
+            onChangeText={(text) => {
+              setUserName(text);
+            }}
+          />
+          <TextInput
+            style={styles.textInput}
+            value={firstName}
+            placeholder={user.firstName ? user.firstName : 'First Name'}
+            onChangeText={(text) => {
+              setFirstName(text);
+            }}
+          />
+          <TextInput
+            style={styles.textInput}
+            value={lastName}
+            placeholder={user.lastName ? user.lastName : 'Last Name'}
+            onChangeText={(text) => {
+              setLastName(text);
+            }}
+          />
+          <TextInput
+            style={styles.textInput}
+            value={address}
+            placeholder={user.address ? user.address : 'Address'}
+            onChangeText={(text) => {
+              setAddress(text);
+            }}
+          />
+          <Button
+            style={styles.button}
+            mode='contained'
+            onPress={handleUpdate}
+            color='#90be6d'
+            contentStyle={{ height: 45 }}
+            labelStyle={{
+              color: 'white',
+              fontSize: 18,
+            }}
+          >
+            Update
+          </Button>
+        </View>
       </View>
-      <Button title="ClickHere" onPress={() => alert("Button Clicked!")} />
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignContent: "center",
+    justifyContent: 'center',
+    // paddingHorizontal: 30,
+    // alignContent: "center",
   },
-  buttons: {
-    alignItems: "center",
+  textInput: {
+    marginTop: 15,
   },
   button: {
     minWidth: 180,
-    marginBottom: 15,
+    marginTop: 15,
   },
-  list: {
-    marginTop: 30,
-    marginBottom: 30,
-  },
+  // buttons: {
+  //   alignItems: 'center',
+  // },
+  // button: {
+  //   minWidth: 180,
+  //   marginBottom: 15,
+  // },
+  // list: {
+  //   marginTop: 30,
+  //   marginBottom: 30,
+  // },
 });
