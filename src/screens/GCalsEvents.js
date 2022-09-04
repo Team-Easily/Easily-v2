deb727dd75debc51face5e8d3c9cd1d150c11f10
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-09-04 15:27:07 -0500

    listing last 10 events on screen

--- src/screens/GCalsEvents.js
+++ src/screens/GCalsEvents.js
@@ -1,71 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, ScrollView, View, StyleSheet } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { setCalendars } from '../components/calendars/calendars';
+import { setEvents } from '../components/events/events';
 import { Headline, Title, List } from 'react-native-paper';
+// import { google } from 'googleapis';
+// import { authenticate } from '@google-cloud/local-auth';
 
 export const GCalsEvents = ({ navigation }) => {
   const user = useSelector((state) => state.auth.currentUser);
   const accessToken = useSelector((state) => state.auth.accessToken);
-  const calendars = useSelector((state) => state.calendars.calendars);
+  const events = useSelector((state) => state.events.events);
   const dispatch = useDispatch();
-
-  const getCalendar = (data) => {
-    let calendar = {
-      id: data.id,
-      body: data.payload.body.data,
-      snippet: data.snippet,
-      threadId: data.threadId,
-    };
-    for (
-      let headerIndex = 0;
-      headerIndex < data.payload.headers.length;
-      headerIndex++
-    ) {
-      if (data.payload.headers[headerIndex].name == 'Subject') {
-        calendar.subject = data.payload.headers[headerIndex].value;
-      }
-      if (data.payload.headers[headerIndex].name == 'From') {
-        calendar.from = data.payload.headers[headerIndex].value;
-      }
-      if (data.payload.headers[headerIndex].name == 'Date') {
-        calendar.date = data.payload.headers[headerIndex].value;
-      }
-    }
-    return calendar;
-  };
-
-  getEvents = (data) => {
-    const event = {
-      calendarId: 'primary',
-      timeMin: new Date().toISOString(),
-      showDeleted: false,
-      singleEvents: true,
-      maxResults: 10,
-      orderBy: 'startTime',
-    };
-
-    const events = response.result.items;
-    if (!events || events.length == 0) {
-      // document.getElementById('content').innerText = 'No events found.';
-      return;
-    }
-    // Flatten to string to display
-    const output = events.reduce(
-      (str, event) =>
-        `${str}${event.summary} (${
-          event.start.dateTime || event.start.date
-        })\n`,
-      'Events:\n'
-    );
-    // document.getElementById('content').innerText = output;
-  };
 
   useEffect(() => {
     if (user.uid) {
       const fetchEventsFromCalendar = async () => {
         try {
-          let uri = `https://www.googleapis.com/calendar/v3/calendars/primary/events/`;
+          let timeMin = new Date().toISOString();
+          let apiKey = 'AIzaSyBWu0w9cYaNXv2bGEf0dUOWNUdGwwoRREw';
+          let uri = `https://www.googleapis.com/calendar/v3/calendars/primary/events?maxResults=10&orderBy=startTime&singleEvents=true&timeMin=${timeMin}&key=${apiKey}`;
+
           const res = await fetch(uri, {
             headers: {
               Authorization: `Bearer ${accessToken}`,
@@ -73,30 +27,21 @@ export const GCalsEvents = ({ navigation }) => {
             },
           });
           const data = await res.json();
+          console.log(data);
+          const events = data.items;
+          if (!events || events.length === 0) {
+            console.log('No upcoming events found.');
+            return;
+          }
+          console.log('Upcoming 10 events:');
           const eventsArr = [];
-          data.events.forEach(async (event) => {
-            let uri = `https://www.googleapis.com/calendar/v3/calendars/primary/events/${event.id}?key=${accessToken}`;
-
-            const res = await fetch(uri, {
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-                Accept: `application/json`,
-              },
-            });
-            const data = await res.json();
+          events.map((event, i) => {
+            eventsArr.push(event);
+            const start = event.start.dateTime || event.start.date;
+            console.log(`${start} - ${event.summary}`);
           });
-          console.log('DATA: ', data);
 
-          //   if (
-          //     data.labelIds.includes('INBOX') &&
-          //     data.labelIds.includes('UNREAD')
-          //   ) {
-          //     const calendar = getCalendar(data);
-          //     calendarsArr.push(calendar);
-          //   }
-          //   dispatch(setCalendars(calendarsArr));
-          // });
-          // console.log(calendarsArr);
+          dispatch(setEvents(eventsArr));
         } catch (error) {
           console.log(error);
         }
@@ -105,9 +50,10 @@ export const GCalsEvents = ({ navigation }) => {
     }
   }, [user]);
 
-  // const from = (calendar) => {
-  //   return 'From: ' + calendar.from;
-  // };
+  const details = (event) => {
+    const start = event.start.dateTime || event.start.date;
+    return start;
+  };
   // const subject = (calendar) => {
   //   return 'Subject: ' + calendar.subject;
   // };
@@ -115,25 +61,24 @@ export const GCalsEvents = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
-        {calendars.length !== 0 ? (
+        {events.length !== 0 ? (
           <View>
             <Headline style={styles.headline}>
-              {calendars.length} calendars waiting
+              {events.length} upcoming events
             </Headline>
             <List.Section>
-              {!!calendars.length &&
-                calendars.map((calendar) => {
+              {!!events.length &&
+                events.map((event) => {
                   return (
                     <List.Accordion
-                      key={calendar.id}
-                      title={from(calendar)}
-                      description={subject(calendar)}
-                      left={(props) => <List.Icon {...props} icon='gmail' />}
+                      key={event.id}
+                      title={event.summary}
+                      description={details(event)}
+                      left={(props) => (
+                        <List.Icon {...props} icon='calendar-clock' />
+                      )}
                     >
-                      <List.Item
-                        title={calendar.snippet}
-                        titleNumberOfLines='4'
-                      />
+                      {/* <List.Item title={event.snippet} titleNumberOfLines='4' /> */}
                     </List.Accordion>
                   );
                 })}
@@ -141,7 +86,7 @@ export const GCalsEvents = ({ navigation }) => {
           </View>
         ) : (
           <View>
-            <Headline style={styles.headline}>No calendars to show.</Headline>
+            <Headline style={styles.headline}>No events to show.</Headline>
             <Title style={styles.headline}>Did you Login with Google?</Title>
           </View>
         )}
--- src/store.js
+++ src/store.js
@@ -2,14 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './components/auth/authSlice';
 import todosReducer from './components/todos/todoSlice';
 import emailsReducer from './components/emails/emails';
-import emailsReducer from './components/calendars/calendars';
+import eventsReducer from './components/events/events';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     todos: todosReducer,
     emails: emailsReducer,
-    calendars: calendarsReducer,
+    events: eventsReducer,
   },
 });
 
